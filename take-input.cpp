42c1e23cd00826124255460f80bd73247d410101
aditya sahu <32958570+aditya-sahu@users.noreply.github.com>
2018-02-12 22:59:20 +0530

    Create take-input.cpp
    Takes input till there is no input

---
+++ take-input.cpp
@@ -0,0 +1,10 @@
+//Takes input till there is no inupt 
+#include<iostream>
+using namespace std;
+int main()
+{
+        int i;
+        while(cin>>i)
+            cout<<i,cout<<i+1;
+        return 0;
+}
